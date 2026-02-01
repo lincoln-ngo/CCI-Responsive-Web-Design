b1f67a5aa0774b2a54e6a5429bb59d7aa0154bda
lincoln-ngo <lincoln.hl.ngo@gmail.com>
2026-02-01 16:55:09 +1100

    Added subnavigation lists. CSS styling of these lists are not yet complete.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function showNavigationMenu() {
-    const navMenu = document.querySelector("nav ul");
+// Code that controls the toggle navigation button on small screens
+
+function showParentNavigationMenu() {
+    const navMenu = document.querySelector("nav ul.parent-navigation-list");
     navMenu.classList.toggle("active");
 }
 
 const navToggleButton = document.querySelector(".nav-toggle-button");
-navToggleButton.addEventListener("click", showNavigationMenu);
+navToggleButton.addEventListener("click", showParentNavigationMenu);
+
+
+// Code that controls the subnavigation toggles
+
+const parentNavItems = document.querySelectorAll(".contains-child-navigation-list");
+
+for (const item of parentNavItems) {
+    const button = item.querySelector("button");
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        item.classList.toggle("active");
+    }
+    )
+};
