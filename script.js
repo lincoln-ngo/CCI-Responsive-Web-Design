2cac736978bf9a0ac27a43ad0ed3076af9d1f37d
lincoln-ngo <lincoln.hl.ngo@gmail.com>
2026-02-02 10:55:42 +1100

    Updated navigation menu to be an exclusive accordion

--- script.js
+++ script.js
@@ -15,9 +15,21 @@ const parentNavItems = document.querySelectorAll(".contains-child-navigation-lis
 
 for (const item of parentNavItems) {
     const button = item.querySelector("button");
+    const icon = button.querySelector("span.fa")
     button.addEventListener("click", (e) => {
         e.preventDefault();
+
+        const currentlyOpenSection = document.querySelector(".contains-child-navigation-list.active");
+        const currentlyOpenSectionIcon = document.querySelector(".contains-child-navigation-list.active button span.fa");
+        if (currentlyOpenSection && item !== currentlyOpenSection) {
+            currentlyOpenSection.classList.remove("active");
+            currentlyOpenSectionIcon.classList.toggle("fa-minus");
+            currentlyOpenSectionIcon.classList.toggle("fa-plus");
+        }
+
         item.classList.toggle("active");
+        icon.classList.toggle("fa-plus");
+        icon.classList.toggle("fa-minus");
     }
     )
 };
